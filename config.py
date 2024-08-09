978da57c79f6abb3a8c4acf243298922c90fc67a
HaTiWinter <hatiwinter@yeah.net>
2024-08-09 15:29:16 +0800

    fix

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
         self.gr_max_size = int(os.environ.get("max_size", 1024))
         self.gr_default_concurrency_limit = int(os.environ.get("default_concurrency_limit", 512))
         self.gr_is_inbrowser = False if os.environ.get("is_inbrowser", "True").lower() == "false" else True
-        self.gr_is_quiet = True if os.environ.get("is_quiet", "False").lower() == "false" else False
+        self.gr_is_quiet = True if os.environ.get("is_quiet", "False").lower() == "true" else False
         self.gr_is_share = True if os.environ.get("is_share", "False").lower() == "true" else False
         self.gr_server_name = "0.0.0.0"
 
